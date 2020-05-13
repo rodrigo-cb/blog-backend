0e91e93cb2349c69c28908dafba997e4e7600044
Rodrigo Cavero Blades <rodrigo.a.cavero.blades.21@dartmouth.edu>
2020-05-12 22:05:02 -0400

    fixed minor bug

--- src/controllers/post_controller.js
+++ src/controllers/post_controller.js
@@ -19,33 +19,39 @@ export const createPost = (req, res) => {
 };
 export const getPosts = (req, res) => {
   Post.find().exec((err, posts) => {
-    res.send(posts);
+    if (err) {
+      res.status(500).json({ err });
+    }
+    res.json(posts);
   });
 };
 
 export const getPost = (req, res) => {
   Post.findById(req.params.id).exec((err, post) => {
-    res.send(post);
+    res.json(post);
   });
 };
 
 export const deletePost = (req, res) => {
   Post.remove({ _id: req.params.id }).exec((err, post) => {
     if (err) {
-      res.send(err);
+      res.status(500).json({ err });
     }
-    res.send({ message: 'post deleted' });
+    res.json({ message: 'post deleted' });
   });
 };
 export const updatePost = (req, res) => {
   Post.findById(req.params.id).exec((err, post) => {
+    if (err) {
+      res.status(500).json({ err });
+    }
     post.title = req.body.title;
     post.tags = req.body.tags;
     post.content = req.body.content;
     post.coverUrl = req.body.coverUrl;
 
     post.save()
-      .then(res.send(post))
+      .then(res.json(post))
       .catch((error) => {
         res.status(500).json({ error });
       });
